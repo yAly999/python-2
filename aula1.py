4a36118efbbaac6fee1dd67f515ea7169d61696b
yAly999 <alysson.delpawlack@gmail.com>
2026-06-02 14:19:27 -0300

    ...

--- aula1.py
+++ aula1.py
@@ -1,14 +1,15 @@
-# Nosso banco de dados
-
 usuarios = [
     ["alice", "Alice@gmail.com", 30], 
     ["alysson", "alysson@gmail.com", 19], 
     ["carlos", "carlos@gmail.com", 9], 
     ["leonardo", "leonardo@gmail.com", 67], 
     ["caua", "caua@gmail.com", 22], 
-    ["joao", "joao@gmail.com", 18], 
-    ...
+    ["joao", "joao@gmail.com", 18]
 ]
 
-for usuarios in usuarios:
-    print(usuarios)
+# 1. O correto é usar "usuario" no singular para não destruir a lista original
+for usuario in usuarios:
+    print(usuario)
+
+# 2. Uso correto dos colchetes separados para pegar o e-mail do alysson
+print(usuarios[0][0])
