428ca9373f0e35067010f9af4b4ff9936313dd05
yAly999 <alysson.delpawlack@gmail.com>
2026-06-02 14:10:09 -0300

    ..

---
+++ aula1.py
@@ -0,0 +1,14 @@
+# Nosso banco de dados
+
+usuarios = [
+    ["alice", "Alice@gmail.com", 30], 
+    ["alysson", "alysson@gmail.com", 19], 
+    ["carlos", "carlos@gmail.com", 9], 
+    ["leonardo", "leonardo@gmail.com", 67], 
+    ["caua", "caua@gmail.com", 22], 
+    ["joao", "joao@gmail.com", 18], 
+    ...
+]
+
+for usuarios in usuarios:
+    print(usuarios)
